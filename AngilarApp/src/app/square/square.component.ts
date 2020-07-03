9bd7d15816cc3f7c00e906f2e36ad721d0b682a7
sisidjidji <s.azibi@live.fr>
2020-07-03 01:07:35 -0500

    board and square functions done

--- AngilarApp/src/app/square/square.component.ts
+++ AngilarApp/src/app/square/square.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  templateUrl: './square.component.html',
-  styleUrls: ['./square.component.css']
+  template: `
+  `,
+  styles: []
 })
-export class SquareComponent implements OnInit {
+export class SquareComponent  {
+@Input() value: 'X' | 'O';
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
-
-}
